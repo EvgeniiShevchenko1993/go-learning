c9d1c7d7ff1f91b6c7ea0caa9739608f16ba7bbc
EvgeniiShevchenko1993 <evgeny.a.shevchenko@accenture.com>
2024-05-28 12:26:34 +0200

    update progress REST project

--- REST API/routes/events.go
+++ REST API/routes/events.go
@@ -89,3 +89,28 @@ func updateEvent(context *gin.Context) {
 	context.JSON(http.StatusOK, gin.H{"message": "Event update successfully"})
 
 }
+
+func deleteEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event ID"})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the event"})
+		return
+	}
+
+	err = event.Delete()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Event deleted sucessfully"})
+
+}
